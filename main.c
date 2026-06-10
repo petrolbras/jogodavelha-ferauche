5d0e8960512ed728622f6ae38169ff8d97256c9e
petrol <pietrogouvearepleop@gmail.com>
2026-06-10 15:11:34 -0300

    Mudado a lógica, dividida as funções mas precisa arrumar alguns bugs

--- main.c
+++ main.c
@@ -1,29 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-
-    char jogador_X[100];
-    char jogador_O[100];
-    char vencedor[100];
-
+void jogar (char jogador_X[], char jogador_O[], char mat[3][3]) {
+    
+    char vencedor[100] = "";
     int i, j;
-    char mat[3][3];
-
-    printf("Digite os nome do jogador X:\n");
-
-    if (fgets(jogador_X, sizeof(jogador_X), stdin)) {
-        jogador_X[strcspn(jogador_X, "\n")] = '\0'; 
-    }
-
-    printf("Agora, digite o nome do jogador O:\n");
-
-    if (fgets(jogador_O, sizeof(jogador_O), stdin)) {
-        jogador_O[strcspn(jogador_O, "\n")] = '\0'; 
-    } 
-
-
-    // o strcspn é usado para remover o \n que o fgets coloca
 
     // inicialização do jogo da velha
 
@@ -33,27 +14,31 @@ int main() {
         }
     }
 
-    while (vencedor[0] == '\0') {
-
+    while(vencedor[0] == '\0') {
         // jogador X
 
         int linha, coluna;
 
         printf("Vez de %s:\n", jogador_X);
 
-        printf("%s, informe linha: %d\n", jogador_X, linha);
-
+        printf("%s, informe linha: \n", jogador_X);
         scanf("%d", &linha);
 
-        printf("%s, informe coluna: %d\n", jogador_X, coluna);
-
+        printf("%s, informe coluna: \n", jogador_X);
         scanf("%d", &coluna);
 
+        //arrumar
+
         while (mat[linha][coluna] != ' ') {
+            if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
+                printf("Linha e coluna devem ser entre 0 e 2. Informe nova posição!.\n");
+            }
+
             printf("Posição linha %d e coluna %d já ocupada. Informe nova posição!.\n", linha, coluna);
 
             scanf("%d", &linha);
             scanf("%d", &coluna);
+
         }
 
         mat[linha][coluna] = 'X';
@@ -91,15 +76,21 @@ int main() {
 
         printf("Vez de %s:\n", jogador_O);
 
-        printf("%s, informe linha: %d\n", jogador_O, linha);
+        printf("%s, informe linha: \n", jogador_O, linha);
 
         scanf("%d", &linha);
 
-        printf("%s, informe coluna: %d\n", jogador_O, coluna);
+        printf("%s, informe coluna: \n", jogador_O, coluna);
 
         scanf("%d", &coluna);
 
+        //arrumar
+
         while (mat[linha][coluna] != ' ') {
+            if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
+                printf("Linha e coluna devem ser entre 0 e 2. Informe nova posição!.\n");
+            }
+
             printf("Posição linha %d e coluna %d já ocupada. Informe nova posição!.\n", linha, coluna);
 
             scanf("%d", &linha);
@@ -145,55 +136,73 @@ int main() {
             printf("%s e %s empataram.\n", jogador_X, jogador_O);
             break;
         }
+    }
+}
 
+void perguntar_nomes(char jogador_X[100], char jogador_O[100], int tamanho) {
+
+    // o strcspn é usado para remover o \n que o fgets coloca
+
+    printf("Digite o nome do jogador X:\n");
+
+    if (fgets(jogador_X, tamanho, stdin)) {
+        jogador_X[strcspn(jogador_X, "\n")] = '\0'; 
     }
 
-    // perguntar se o usuário quer jogar novamente e também se são os mesmos jogadores
+    printf("Agora, digite o nome do jogador O:\n");
 
-    char resposta;
-    char mesma_partida;
+    if (fgets(jogador_O, tamanho, stdin)) {
+        jogador_O[strcspn(jogador_O, "\n")] = '\0'; 
+    }
+}
 
-    // resolver isso
+int main() {
 
-    while (!resposta) {
+    char jogador_X[100] = "";
+    char jogador_O[100] = "";
+    char mat[3][3];
 
-        printf("Deseja jogar novamente? (S/N)\n");
+    perguntar_nomes(jogador_X, jogador_O, 100);
 
-        scanf("%c", &resposta);
+    while (1) {
+        
+        jogar(jogador_X, jogador_O, mat);
 
-        if (resposta != 'S' && resposta != 's' && resposta != 'N' && resposta != 'n') {
-            printf("Por favor responda S ou N. Deseja jogar novamente? (S/N)\n");
+        char resposta = '\0';
+        char mesma_partida = '\0';
 
-            scanf("%c", &resposta);
+        do {
+
+            printf("Deseja jogar novamente? (S/N)\n");
+
+            scanf(" %c", &resposta);
+
+            if (resposta && resposta != 'S' && resposta != 's' && resposta != 'N' && resposta != 'n') {
+                printf("Por favor responda S ou N.\n");
+            }
+
+        } while (resposta != 'S' && resposta != 's' && resposta != 'N' && resposta != 'n');
+
+        if (resposta == 'N' || resposta == 'n') {
+            printf("Fim do programa!\n");
+            break;
+        } else if (resposta == 'S' || resposta == 's') {
+            do {
+                printf("Serão os mesmos jogadores? (S/N)\n");
+
+                scanf(" %c", &mesma_partida);
+
+                if (mesma_partida && mesma_partida != 'S' && mesma_partida != 's' && mesma_partida != 'N' && mesma_partida != 'n') {
+                    printf("Por favor responda S ou N.\n");
+                }
+            } while (mesma_partida != 'S' && mesma_partida != 's' && mesma_partida != 'N' && mesma_partida != 'n');
         }
-    }
 
-    //resolver isso tambem e dos nomes
-
-    if (resposta == 'S' || resposta == 's') {
-
-        printf("São os mesmos jogadores? (S/N)\n");
-
-        scanf("%c", &mesma_partida);
-
-        while (mesma_partida != 'S' && mesma_partida != 's' && mesma_partida != 'N' && mesma_partida != 'n') {
-            printf("Por favor responda S ou N. São os mesmos jogadores? (S/N)\n");
-
-            scanf("%c", &mesma_partida);
+        if (mesma_partida == 'N' || mesma_partida == 'n') {
+            getchar(); // limpar o \n
+            perguntar_nomes(jogador_X, jogador_O, 100);
         }
-
-        if (mesma_partida == 'S' || mesma_partida == 's') {
-            vencedor[0] = '\0';
-
-            main();
-        } else {
-
-            main();
-        } 
-    } else {
-        printf("Fim do programa!\n");
-    }
-
+    }    
 
     return 0;
 }
